1a45b955b6dcffc2ac7425558315a9931feef376
jontorregano <jontorregano@gmail.com>
2017-07-11 20:18:35 -0500

    payment line up changes

--- config.php
+++ config.php
@@ -8,8 +8,8 @@
 require_once('vendor/autoload.php');
 
 $stripe = array(
-    "secret_key"      => "sk_test_sS5FtRnx8kdSixHYwzoiMPxa",
-    "publishable_key" => "pk_test_tpYhNaR95tgLDMFFoWuDS7gQ"
+    "secret_key"      => "sk_test_uCXcVCI0XqMGdteK8DsdMkRo",
+    "publishable_key" => "pk_test_x3mhpqgfAQtdtXv3SjlMqkMM"
 );
 
 \Stripe\Stripe::setApiKey($stripe['secret_key']);
